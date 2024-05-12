89f50643fbdcca2ce97a5f54ae3e51dd22370e9a
Yayla Sezginer <93011510+yaylasezginer@users.noreply.github.com>
2024-05-12 09:58:55 -0700

    Add files via upload

---
+++ FRRFread.m
@@ -0,0 +1,19 @@
+
+% Read .csv data outputs and compile into a single .mat dataset
+
+% UPDATE WITH LOCAL DIRECTORY
+
+frrf_dir = '/Users/yaylasez/Desktop/Pearl2024';
+
+files = dir(frrf_dir); % Open data folder and read file names
+data = contains({files.name},'_fit.csv'); % ID data files as .csv 
+fn = {files(data).name}; % store the names of FRRF data files
+
+FRRF = [];
+for i = 1:numel(fn)
+    
+    filepath = [frrf_dir '/' fn{i}];
+    dat = readtable(filepath);
+    FRRF = [FRRF; dat];
+    
+end
